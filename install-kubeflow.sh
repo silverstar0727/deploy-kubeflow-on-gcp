f89a1807c81535e6c9a8a1717342ace52fad085d
JeongMin Do <49096513+silverstar0727@users.noreply.github.com>
2021-04-15 17:48:26 +0900

    Update install-kubeflow.sh

--- install-kubeflow.sh
+++ install-kubeflow.sh
@@ -1,13 +1,11 @@
-# Start minikube
 minikube delete
 minikube start --cpus 6 --memory 12288 --disk-size=120g --driver=none
 sudo chmod 777 -R ~/.kube
 
-# Installation of Kubeflow
 sudo rm -rf ~/kubeflow
 mkdir -p ~/kubeflow/v1.2
 cd ~/kubeflow/v1.2
-# Install kfctl
+
 wget https://github.com/kubeflow/kfctl/releases/download/v1.2.0/kfctl_v1.2.0-0-gbc038f9_linux.tar.gz
 tar -xvf kfctl_v1.2.0-0-gbc038f9_linux.tar.gz
 	
