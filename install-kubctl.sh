1ebe5f11552c39cf7d9215caee6b165341779f1f
JeongMin Do <49096513+silverstar0727@users.noreply.github.com>
2021-04-15 17:48:03 +0900

    Update install-kubctl.sh

--- install-kubctl.sh
+++ install-kubctl.sh
@@ -1,13 +1,9 @@
-# Install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.15.0/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-#sudo chmod 777 -R /usr/local/bin/kubectl
 
-# Install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
-#sudo chmod 777 -R /usr/local/bin/minikube
 
 sudo apt-get install -y conntrack
 
